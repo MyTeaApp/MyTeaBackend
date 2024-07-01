dfd0071c7adb92c7cec807244d1309964ab00548
Sergei Krainyukov <sk-new@mail.ru>
2024-07-01 13:59:24 +0300

    init

---
+++ src/main/java/com/mytea/mytea_backend/MyteaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mytea.mytea_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyteaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyteaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytea/mytea_backend/MyteaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytea.mytea_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyteaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
